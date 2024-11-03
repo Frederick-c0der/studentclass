7fc1cfa0711ed7252c77ecdfedfa3f7f62c672da
Frederick-c0der <frederickchanngyan@gmail.com>
2024-11-03 11:04:01 +0000

    Add files via upload

---
+++ Studentclass.py
@@ -0,0 +1,12 @@
+class Student():
+    def __init__(self):
+        self.name=""
+        self.age=0
+        self.house=""
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("House:", self.house)
+Luke=Student()
+Leia=Student()
+Luke.display()
